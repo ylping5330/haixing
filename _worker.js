b6472cbf4357d1b7838b42698ac02fcf94cc58d8
ylping5330 <yangliping5330@gmail.com>
2025-07-07 20:35:11 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
-    _url.hostname = _url.pathname.startsWith("/gh/") ? "cdn.lasolasbeauty.com" : "lasolasbeauty.com";
+    _url.hostname = _url.pathname.startsWith("/gh/") ? "cdn.lasolasbeauty.com" : "cdn.lasolasbeauty.com";
     const req = new Request(_url, request);
     return fetch(req);
   },
